0284ef40176cc41ff968bd63804175293ea18a45
Emmanuel George <Emmanuelgeorge1000@gmail.com>
2022-09-03 22:48:36 +0530

    Initial Commit

---
+++ WeShare/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeShare
+//
+//  Created by Emmanuel George on 03/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
