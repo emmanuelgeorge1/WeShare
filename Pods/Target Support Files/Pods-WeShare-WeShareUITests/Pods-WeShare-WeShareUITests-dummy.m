90a7aa1dc487a1ce964da1cb321135210d6b921f
Emmanuel George <Emmanuelgeorge1000@gmail.com>
2022-09-26 16:59:56 +0530

    new

---
+++ Pods/Target Support Files/Pods-WeShare-WeShareUITests/Pods-WeShare-WeShareUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WeShare_WeShareUITests : NSObject
+@end
+@implementation PodsDummy_Pods_WeShare_WeShareUITests
+@end
---
+++ Pods/Target Support Files/Pods-WeShareTests/Pods-WeShareTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WeShareTests : NSObject
+@end
+@implementation PodsDummy_Pods_WeShareTests
+@end
